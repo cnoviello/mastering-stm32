a7286eff6a4f3082d9f57bf54b54c3d9b3abe463
Carmine Noviello <cnoviello@hotmail.com>
2015-12-18 17:50:08 +0100

    Completed import of F303RE project

--- nucleo-f303RE/include/mxconstants.h
+++ nucleo-f303RE/include/mxconstants.h
@@ -44,8 +44,8 @@
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
-#define LD2_Pin GPIO_PIN_13
-#define LD2_GPIO_Port GPIOB
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
