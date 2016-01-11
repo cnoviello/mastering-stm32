0e0a5b7ea36f65bd96605130100bd75e0de81d76
Carmine Noviello <cnoviello@hotmail.com>
2016-01-11 08:51:25 +0100

    Added first two examples for chapter 9

---
+++ nucleo-f030R8/src/ch9/uartsend.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import serial, time
+
+SERIAL = "/dev/tty.usbmodem1a1213" #Windows users, replace with "COMx"
+
+ser = serial.Serial(SERIAL, 38400)
+
+while True:
+    ser.write((0xff,))
+    time.sleep(0.05)
+    ser.write((0,))
+    time.sleep(0.05)
+    
+ser.close()
