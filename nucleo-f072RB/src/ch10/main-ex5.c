b379d78cb647bad8d9aa3a285b6606dec48944ba
Carmine Noviello <cnoviello@hotmail.com>
2016-02-05 17:13:26 +0100

    Ported EX10 examples to Nucleo-F072

--- nucleo-f072RB/src/ch10/main-ex5.c
+++ nucleo-f072RB/src/ch10/main-ex5.c
@@ -10,7 +10,7 @@ typedef unsigned long uint32_t;
 #define PERIPH_BASE     0x40000000
 
 /* Work out end of RAM address as initial stack pointer */
-#define SRAM_SIZE       16*1024     // STM32F070RB has 16 KB of RAM
+#define SRAM_SIZE       16*1024     // STM32F072RB has 16 KB of RAM
 #define SRAM_END        (SRAM_BASE + SRAM_SIZE)
 
 /* RCC peripheral addresses applicable to GPIOA */
