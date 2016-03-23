978c75caccfdcc35589a8d09b41a95da661cd7df
Carmine Noviello <cnoviello@hotmail.com>
2016-03-23 17:13:26 +0100

    Completed test of CH11 examples on F072

--- nucleo-f072RB/src/ch11/main-ex3.c
+++ nucleo-f072RB/src/ch11/main-ex3.c
@@ -55,7 +55,8 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base) {
     GPIO_InitStruct.Pin = GPIO_PIN_2;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF0_TIM3;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
  }
 }
--- nucleo-f072RB/src/ch11/nucleo_hal_bsp.c
+++ nucleo-f072RB/src/ch11/nucleo_hal_bsp.c
@@ -116,12 +116,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIOC pin8 */
+  /*Configure GPIO pin : PA8 */
   GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF0_MCO;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
