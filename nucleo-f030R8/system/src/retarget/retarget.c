31018d2590fd0a4cda126cfd04f28678fc88a808
Carmine Noviello <cnoviello@hotmail.com>
2016-04-21 17:33:51 +0200

    Added CH5 example to F410

--- nucleo-f030R8/system/src/retarget/retarget.c
+++ nucleo-f030R8/system/src/retarget/retarget.c
@@ -8,6 +8,8 @@
 #include <../system/include/retarget/retarget.h>
 #include <stdint.h>
 
+#if !defined(OS_USE_SEMIHOSTING)
+
 #define STDIN_FILENO  0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
@@ -23,7 +25,7 @@ void RetargetInit(UART_HandleTypeDef *huart) {
 }
 
 int _isatty(int fd) {
-  if(fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+  if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
     return 1;
 
   errno = EBADF;
@@ -31,16 +33,21 @@ int _isatty(int fd) {
 }
 
 int _write(int fd, char* ptr, int len) {
-  if(fd == STDOUT_FILENO || fd == STDERR_FILENO) {
-    HAL_UART_Transmit(gHuart, (uint8_t *)ptr, len, HAL_MAX_DELAY);
-    return len;
+  HAL_StatusTypeDef hstatus;
+
+  if (fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+    hstatus = HAL_UART_Transmit(gHuart, (uint8_t *) ptr, len, HAL_MAX_DELAY);
+    if (hstatus == HAL_OK)
+      return len;
+    else
+      return EIO;
   }
   errno = EBADF;
   return -1;
 }
 
 int _close(int fd) {
-  if(fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+  if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
     return 0;
 
   errno = EBADF;
@@ -48,25 +55,30 @@ int _close(int fd) {
 }
 
 int _lseek(int fd, int ptr, int dir) {
-  (void)fd;
-  (void)ptr;
-  (void)dir;
+  (void) fd;
+  (void) ptr;
+  (void) dir;
 
   errno = EBADF;
   return -1;
 }
 
 int _read(int fd, char* ptr, int len) {
-  if(fd == STDIN_FILENO) {
-    HAL_UART_Receive(gHuart, (uint8_t *)ptr, len, HAL_MAX_DELAY);
-    return len;
+  HAL_StatusTypeDef hstatus;
+
+  if (fd == STDIN_FILENO) {
+    hstatus = HAL_UART_Receive(gHuart, (uint8_t *) ptr, 1, HAL_MAX_DELAY);
+    if (hstatus == HAL_OK)
+      return 1;
+    else
+      return EIO;
   }
   errno = EBADF;
   return -1;
 }
 
 int _fstat(int fd, struct stat* st) {
-  if(fd >= STDIN_FILENO && fd <= STDERR_FILENO) {
+  if (fd >= STDIN_FILENO && fd <= STDERR_FILENO) {
     st->st_mode = S_IFCHR;
     return 0;
   }
@@ -74,3 +86,5 @@ int _fstat(int fd, struct stat* st) {
   errno = EBADF;
   return 0;
 }
+
+#endif //#if !defined(OS_USE_SEMIHOSTING)
