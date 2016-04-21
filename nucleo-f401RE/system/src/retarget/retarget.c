4d4ec39e2c1590d5a5bf993a97b1ea49d2e34b5f
Carmine Noviello <cnoviello@hotmail.com>
2016-04-21 17:01:42 +0200

    Fixed the way the _read() work in retarget.c

--- nucleo-f401RE/system/src/retarget/retarget.c
+++ nucleo-f401RE/system/src/retarget/retarget.c
@@ -25,7 +25,7 @@ void RetargetInit(UART_HandleTypeDef *huart) {
 }
 
 int _isatty(int fd) {
-  if(fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+  if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
     return 1;
 
   errno = EBADF;
@@ -33,16 +33,21 @@ int _isatty(int fd) {
 }
 
 int _write(int fd, char* ptr, int len) {
-  if(fd == STDOUT_FILENO || fd == STDERR_FILENO) {
-    HAL_UART_Transmit(gHuart, (uint8_t *)ptr, len, HAL_MAX_DELAY);
-    return len;
+  HAL_StatusTypeDef hstatus;
+
+  if (fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+    hstatus = HAL_UART_Transmit(gHuart, (uint8_t *) ptr, len, HAL_MAX_DELAY);
+    if (hstatus == HAL_OK)
+      return len;
+    else
+      return EIO;
   }
   errno = EBADF;
   return -1;
 }
 
 int _close(int fd) {
-  if(fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+  if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
     return 0;
 
   errno = EBADF;
@@ -50,25 +55,30 @@ int _close(int fd) {
 }
 
 int _lseek(int fd, int ptr, int dir) {
-  (void)fd;
-  (void)ptr;
-  (void)dir;
+  (void) fd;
+  (void) ptr;
+  (void) dir;
 
   errno = EBADF;
   return -1;
 }
 
 int _read(int fd, char* ptr, int len) {
-  if(fd == STDIN_FILENO) {
-    HAL_UART_Receive(gHuart, (uint8_t *)ptr, len, HAL_MAX_DELAY);
-    return len;
+  HAL_StatusTypeDef hstatus;
+
+  if (fd == STDIN_FILENO) {
+    hstatus = HAL_UART_Receive(gHuart, (uint8_t *) ptr, 1, HAL_MAX_DELAY);
+    if (hstatus == HAL_OK)
+      return 1;
+    else
+      return EIO;
   }
   errno = EBADF;
   return -1;
 }
 
 int _fstat(int fd, struct stat* st) {
-  if(fd >= STDIN_FILENO && fd <= STDERR_FILENO) {
+  if (fd >= STDIN_FILENO && fd <= STDERR_FILENO) {
     st->st_mode = S_IFCHR;
     return 0;
   }
