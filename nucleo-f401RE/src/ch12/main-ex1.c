f9a4061f491cdc4cdd0f06e4d9f6ae700cb0d0fe
Carmine Noviello <cnoviello@hotmail.com>
2016-05-16 18:15:01 +0200

    Completed first example of CH12

--- nucleo-f401RE/src/ch12/main-ex1.c
+++ nucleo-f401RE/src/ch12/main-ex1.c
@@ -8,10 +8,7 @@ extern UART_HandleTypeDef huart2;
 ADC_HandleTypeDef hadc1;
 
 /* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
-static void MX_TIM2_Init(void);
 
 int main(void) {
 
@@ -23,35 +20,23 @@ int main(void) {
 
   HAL_ADC_Start(&hadc1);
 
-  uint16_t startValue = 0;
-
   while (1) {
     char msg[20];
-    uint16_t adcValue;
+    uint16_t rawValue;
     float temp;
-
-    uint16_t *TS_CAL1 = 0x1FFF7A2C;
-    uint16_t *TS_CAL2 = 0x1FFF7A2E;
 
     HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
 
-    adcValue = ((HAL_ADC_GetValue(&hadc1)) / 4095.0) * 3300.0;
-//    adcValue = 80/(*TS_CAL2-*TS_CAL1)*(adcValue-*TS_CAL1) + 30;
-    temp = (adcValue - 760.0) / 2.5;
+    rawValue = ((HAL_ADC_GetValue(&hadc1)) / 4095.0) * 3300.0;
+    temp = (rawValue - 760.0) / 2.5;
     temp += 25.0;
 
-    if(startValue == 0)
-      startValue = adcValue;
-
-    sprintf(msg, "adcValue: %lu\r\n", adcValue);
+    sprintf(msg, "rawValue: %hu\r\n", rawValue);
     HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen(msg), HAL_MAX_DELAY);
 
-    sprintf(msg, "Temp: %f\r\n", temp);
+    sprintf(msg, "Temperature: %f\r\n", temp);
     HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen(msg), HAL_MAX_DELAY);
-
-    HAL_Delay(1000);
   }
-
 }
 
 /* ADC1 init function */
@@ -64,7 +49,7 @@ void MX_ADC1_Init(void) {
   /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
    */
   hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
   hadc1.Init.ScanConvMode = DISABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
@@ -80,9 +65,8 @@ void MX_ADC1_Init(void) {
    */
   sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
   sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_144CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
   HAL_ADC_ConfigChannel(&hadc1, &sConfig);
-
 }
 
 
