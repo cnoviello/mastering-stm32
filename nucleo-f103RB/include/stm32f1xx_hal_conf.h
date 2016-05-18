66de40af5a8024e65d1e65d355365c9b822b3084
Carmine Noviello <cnoviello@hotmail.com>
2016-05-18 14:13:58 +0200

    Ported CH12 examples to F103

--- nucleo-f103RB/include/stm32f1xx_hal_conf.h
+++ nucleo-f103RB/include/stm32f1xx_hal_conf.h
@@ -50,12 +50,12 @@
   * @brief This is the list of modules to be used in the HAL driver 
   */
 #define HAL_MODULE_ENABLED  
-//#define HAL_ADC_MODULE_ENABLED   
+#define HAL_ADC_MODULE_ENABLED
 //#define HAL_CAN_MODULE_ENABLED   
 //#define HAL_CEC_MODULE_ENABLED   
 //#define HAL_CORTEX_MODULE_ENABLED   
 //#define HAL_CRC_MODULE_ENABLED   
-//#define HAL_DAC_MODULE_ENABLED   
+#define HAL_DAC_MODULE_ENABLED
 //#define HAL_DMA_MODULE_ENABLED   
 //#define HAL_ETH_MODULE_ENABLED   
 //#define HAL_FLASH_MODULE_ENABLED   
---
+++ nucleo-f103RB/src/ch12/main-ex1.c
@@ -0,0 +1,99 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+#include <nucleo_hal_bsp.h>
+#include <string.h>
+
+/* Private variables ---------------------------------------------------------*/
+extern UART_HandleTypeDef huart2;
+ADC_HandleTypeDef hadc1;
+
+/* Private function prototypes -----------------------------------------------*/
+static void MX_ADC1_Init(void);
+
+int main(void) {
+
+  HAL_Init();
+  Nucleo_BSP_Init();
+
+  /* Initialize all configured peripherals */
+  MX_ADC1_Init();
+
+  HAL_ADCEx_Calibration_Start(&hadc1);
+
+  HAL_ADC_Start(&hadc1);
+
+  while (1) {
+    char msg[20];
+    uint16_t rawValue;
+    float temp;
+
+    HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+
+    rawValue = HAL_ADC_GetValue(&hadc1);
+    temp = ((float)rawValue) / 4095 * 3300;
+    temp = ((temp - 1400.0) / 4.3) + 25;
+
+    sprintf(msg, "rawValue: %hu\r\n", rawValue);
+    HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen(msg), HAL_MAX_DELAY);
+
+    sprintf(msg, "Temperature: %f\r\n", temp);
+    HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen(msg), HAL_MAX_DELAY);
+  }
+}
+
+/* ADC1 init function */
+void MX_ADC1_Init(void) {
+  ADC_ChannelConfTypeDef sConfig;
+
+  /* Enable ADC peripheral */
+  __HAL_RCC_ADC1_CLK_ENABLE();
+
+  /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+   */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.NbrOfConversion = 1;
+  HAL_ADC_Init(&hadc1);
+
+    /**Configure Regular Channel
+    */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
+  HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+}
+
+
+#ifdef USE_FULL_ASSERT
+
+/**
+ * @brief Reports the name of the source file and the source line number
+ * where the assert_param error has occurred.
+ * @param file: pointer to the source file name
+ * @param line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
