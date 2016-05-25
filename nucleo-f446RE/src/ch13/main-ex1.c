5c9e111ac6ad9f4f55b09d3cf5b9ed792f37b67a
Carmine Noviello <cnoviello@hotmail.com>
2016-05-25 17:46:55 +0200

    Ported CH13 examples to F446

---
+++ nucleo-f446RE/src/ch13/main-ex1.c
@@ -0,0 +1,92 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include <nucleo_hal_bsp.h>
+#include <string.h>
+#include <math.h>
+
+/* Private variables ---------------------------------------------------------*/
+DAC_HandleTypeDef hdac;
+
+/* Private function prototypes -----------------------------------------------*/
+static void MX_DAC_Init(void);
+
+int main(void) {
+  HAL_Init();
+  Nucleo_BSP_Init();
+
+  /* Initialize all configured peripherals */
+  MX_DAC_Init();
+
+  HAL_DAC_Init(&hdac);
+  HAL_DAC_Start(&hdac, DAC_CHANNEL_2);
+
+  while(1) {
+    int i = 2000;
+    while(i < 4000) {
+      HAL_DAC_SetValue(&hdac, DAC_CHANNEL_2, DAC_ALIGN_12B_R, i);
+      HAL_Delay(1);
+      i+=4;
+    }
+
+    while(i > 2000) {
+      HAL_DAC_SetValue(&hdac, DAC_CHANNEL_2, DAC_ALIGN_12B_R, i);
+      HAL_Delay(1);
+      i-=4;
+    }
+  }
+}
+
+/* DAC init function */
+void MX_DAC_Init(void) {
+  DAC_ChannelConfTypeDef sConfig;
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  __HAL_RCC_DAC_CLK_ENABLE();
+
+  /* DAC Initialization  */
+  hdac.Instance = DAC;
+  HAL_DAC_Init(&hdac);
+
+  /**DAC channel OUT2 config */
+  sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
+  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+  HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_2);
+
+  /* DAC GPIO Configuration
+     PA5     ------> DAC_OUT2
+  */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+ * @brief Reports the name of the source file and the source line number
+ * where the assert_param error has occurred.
+ * @param file: pointer to the source file name
+ * @param line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ nucleo-f446RE/src/ch13/main-ex2.c
@@ -0,0 +1,127 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include <nucleo_hal_bsp.h>
+#include <string.h>
+#include <math.h>
+
+#define PI    3.14159
+#define SAMPLES 200
+
+/* Private variables ---------------------------------------------------------*/
+DAC_HandleTypeDef hdac;
+TIM_HandleTypeDef htim6;
+DMA_HandleTypeDef hdma_dac1;
+
+/* Private function prototypes -----------------------------------------------*/
+static void MX_DAC_Init(void);
+static void MX_TIM6_Init(void);
+
+int main(void) {
+  uint16_t IV[SAMPLES], value;
+
+  HAL_Init();
+  Nucleo_BSP_Init();
+
+  /* Initialize all configured peripherals */
+  MX_TIM6_Init();
+  MX_DAC_Init();
+
+  for (uint16_t i = 0; i < SAMPLES; i++) {
+    value = (uint16_t) rint((sinf(((2*PI)/SAMPLES)*i)+1)*2048);
+    IV[i] = value < 4096 ? value : 4095;
+  }
+
+  HAL_DAC_Init(&hdac);
+  HAL_TIM_Base_Start(&htim6);
+  HAL_DAC_Start_DMA(&hdac, DAC_CHANNEL_1, (uint32_t*)IV, SAMPLES, DAC_ALIGN_12B_R);
+
+  while(1);
+}
+
+/* DAC init function */
+void MX_DAC_Init(void) {
+  DAC_ChannelConfTypeDef sConfig;
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  __HAL_RCC_DAC_CLK_ENABLE();
+
+  /**DAC Initialization  */
+  hdac.Instance = DAC;
+  HAL_DAC_Init(&hdac);
+
+  /**DAC channel OUT1 config */
+  sConfig.DAC_Trigger = DAC_TRIGGER_T6_TRGO;
+  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+  HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1);
+
+  /**DAC GPIO Configuration
+  PA4     ------> DAC_OUT1
+  */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* Peripheral DMA init*/
+  hdma_dac1.Instance = DMA1_Stream5;
+  hdma_dac1.Init.Channel = DMA_CHANNEL_7;
+  hdma_dac1.Init.Direction = DMA_MEMORY_TO_PERIPH;
+  hdma_dac1.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_dac1.Init.MemInc = DMA_MINC_ENABLE;
+  hdma_dac1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+  hdma_dac1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+  hdma_dac1.Init.Mode = DMA_CIRCULAR;
+  hdma_dac1.Init.Priority = DMA_PRIORITY_LOW;
+  hdma_dac1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+  HAL_DMA_Init(&hdma_dac1);
+
+  __HAL_LINKDMA(&hdac,DMA_Handle1,hdma_dac1);
+}
+
+
+/* TIM6 init function */
+void MX_TIM6_Init(void) {
+  TIM_MasterConfigTypeDef sMasterConfig;
+
+  __HAL_RCC_TIM6_CLK_ENABLE();
+
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 0;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 8999;
+  HAL_TIM_Base_Init(&htim6);
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig);
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+ * @brief Reports the name of the source file and the source line number
+ * where the assert_param error has occurred.
+ * @param file: pointer to the source file name
+ * @param line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
