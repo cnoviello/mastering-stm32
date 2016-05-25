cf651704d074f7bb5093a8f537d0d4d246762481
Carmine Noviello <cnoviello@hotmail.com>
2016-05-25 16:42:14 +0200

    Completed CH13 examples on F072

---
+++ nucleo-f072RB/src/ch13/stm32f0xx_it.c
@@ -0,0 +1,51 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+#include "stm32f0xx.h"
+#include "stm32f0xx_it.h"
+
+/* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_dac_ch1;
+extern DAC_HandleTypeDef hdac;
+extern TIM_HandleTypeDef htim6;
+
+/******************************************************************************/
+/*            Cortex-M0 Processor Interruption and Exception Handlers         */ 
+/******************************************************************************/
+
+/**
+* @brief This function handles System tick timer.
+*/
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32F0xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f0xx.s).                    */
+/******************************************************************************/
+
+#ifdef USE_DMA
+
+void DMA1_Channel2_3_IRQHandler(void)
+{
+  HAL_DMA_IRQHandler(&hdma_dac_ch1);
+}
+
+//void TIM6_DAC_IRQHandler(void)
+//{
+//  HAL_TIM_IRQHandler(&htim6);
+//  HAL_DAC_IRQHandler(&hdac);
+//}
+#endif
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
