b290785ced27bc4e241a82aed1359de0b53d7747
Carmine Noviello <cnoviello@hotmail.com>
2016-07-13 18:11:43 +0200

    The bootloader is almost complete

--- nucleo-f401RE/src/ch16/flasher.py
+++ nucleo-f401RE/src/ch16/flasher.py
@@ -30,7 +30,7 @@ class TimeoutError(Exception):
 class STM32Flasher(object):
     def __init__(self, serialPort, baudrate=38400):
         self.serial = serial.Serial(serialPort, baudrate=baudrate, timeout=20)
-        self.inProgramMode = False
+        # self.inProgramMode = False
 
     def _crc_stm32(self, data):
         crc = 0xFFFFFFFF
@@ -45,31 +45,20 @@ class STM32Flasher(object):
 
         return (crc & 0xFFFFFFFF)
 
+    def _create_cmd_message(self, msg):
+        cmd = list(msg) + list(struct.pack("I", self._crc_stm32(msg)))
+        # print(cmd)
+        return cmd
+
     def _encryptMessage(self, data):
-        print("DAAAA: ", data)
         data = AESEncryptor.encrypt("".join(map(chr,data)))
         data32 = struct.unpack("IIII", data)
         data += struct.pack("I", self._crc_stm32(data32))
         return data
 
-    def enterProgramMode(self):
-        self.serial.flushInput()        
-        self.serial.write((CMD_ENTER_PROGRAM_MODE,))
-        data = self.serial.read(1)
-        if len(data) == 1:
-            if struct.unpack("b", data)[0] == ACK:
-                self.inProgramMode = True
-            else:
-                raise ProgramModeError("Can't enter in program mode. Try to reset MCU")
-        else:
-            raise TimeoutError("Timeout error")
-
-    def eraseFLASH(self):
-        if not self.inProgramMode:
-            raise ProgramModeError("MCU not in program mode")           
-
+    def eraseFLASH(self, nsectors=0xFF):
         self.serial.flushInput()
-        self.serial.write((CMD_ERASE,))
+        self.serial.write(self._create_cmd_message((CMD_ERASE,nsectors)))
         data = self.serial.read(1)
         if len(data) == 1:
             if struct.unpack("b", data)[0] != ACK:
@@ -78,72 +67,70 @@ class STM32Flasher(object):
             raise TimeoutError("Timeout error")
      
     def getID(self):
-        if not self.inProgramMode:
-            raise ProgramModeError("MCU not in program mode")           
-
-        self.serial.flushInput()            
-        self.serial.write((CMD_GETID,))
-        data = self.serial.read(2)
-        if len(data) == 2:
-            return struct.unpack("h", data)[0]
+        self.serial.flushInput()
+        self.serial.write(self._create_cmd_message((CMD_GETID,)))
+        data = self.serial.read(1)
+        if len(data) == 1:
+            if struct.unpack("b", data)[0] != ACK:
+                raise ProgramModeError("CMD_GETID failed")
+            else:
+                data = self.serial.read(2)
+                if len(data) == 2:
+                    return struct.unpack("h", data)[0]
+                raise ProgramModeError("CMD_GETID failed") 
         else:
             raise TimeoutError("Timeout error")
 
     def writeImage(self, filename):
-        if not self.inProgramMode:
-            raise ProgramModeError("MCU not in program mode")           
-
         ih = IntelHex()  
         ih.loadhex(filename)
         yield {"saddr": ih.minaddr(), "eaddr": ih.maxaddr()}
 
-        self.serial.flushInput()
-        self.serial.write((CMD_WRITE,))
-        data = self.serial.read(1)
-        if len(data) == 1:
-            if struct.unpack("b", data)[0] != ACK:
-                print("Received: ", hex(ord(data)))
-                raise ProgramModeError("Write abort")
-        else:
-            raise TimeoutError("Timeout error")
-
-
         addr = ih.minaddr()
         content = ih.todict()
+        abort = False
+        resend = 0
         while addr <= ih.maxaddr():
-            data = []
-            saddr = addr
-            for i in range(16):
-              try:
-                  data.append(content[addr])
-              except KeyError:
-                  data.append(0xFF)
-              addr+=1
+            if not resend:
+                data = []
+                saddr = addr
+                for i in range(16):
+                  try:
+                      data.append(content[addr])
+                  except KeyError:
+                      data.append(0xFF)
+                  addr+=1
+            try:
+                if resend >= 3:
+                     abort = True
+                     break
 
-            print(map(chr,data))
-            data = self._encryptMessage(data)
-            print(map(ord,data))
-            self.serial.flushInput()
-            self.serial.write(data)
-            data = self.serial.read(1)
-            if len(data) == 1:
-                if struct.unpack("b", data)[0] != ACK:
-                    raise ProgramModeError("Write abort")
-            else:
-                raise TimeoutError("Timeout error")
+                self.serial.flushInput()
+                self.serial.write(self._create_cmd_message([CMD_WRITE] + map(ord, struct.pack("I", saddr))))
+                ret = self.serial.read(1)
+                if len(ret) == 1:
+                    if struct.unpack("b", ret)[0] != ACK:
+                        raise ProgramModeError("Write abort")
+                else:
+                    raise TimeoutError("Timeout error")
 
-            yield {"loc": saddr}
+                encdata = self._encryptMessage(data)
+                self.serial.flushInput()
+                self.serial.write(encdata)
+                ret = self.serial.read(1)
+                if len(ret) == 1:
+                    if struct.unpack("b", ret)[0] != ACK:
+                        raise ProgramModeError("Write abort")
+                else:
+                    raise TimeoutError("Timeout error")
 
-        data = self._encryptMessage([0]*16)
-        self.serial.flushInput()
-        self.serial.flushOutput()        
-        self.serial.write(data)
-        data = self.serial.read(1)
-        if len(data) == 1:
-            if struct.unpack("b", data)[0] != ACK:
-                raise ProgramModeError("Write abort")
-        else:
-            raise TimeoutError("Timeout error")
+                yield {"loc": saddr, "resend": resend}
+                resend = 0
+            except (TimeoutError, ProgramModeError):
+                resend +=1
+
+        yield {"success": not abort}
+
 
 if __name__ == '__main__':
     eraseDone = 0
@@ -151,12 +138,12 @@ if __name__ == '__main__':
     def doErase(arg):
         global eraseDone
 
-        flasher.eraseFLASH()
+        flasher.eraseFLASH(1)
         time.sleep(1)
         eraseDone = 1
 
     flasher = STM32Flasher(sys.argv[1])
-    flasher.enterProgramMode()
+    # flasher.enterProgramMode()
     id = flasher.getID()
     print("STM32 MCU Type: ", STM32_TYPE[id])
     print("Erasing Flash memory", end="")
@@ -166,7 +153,6 @@ if __name__ == '__main__':
         sys.stdout.flush()
         time.sleep(0.5)
 
-    # flasher.eraseFLASH()
     print(" Done")
     print("Loading %s HEX file...." % sys.argv[2])
 
@@ -175,42 +161,14 @@ if __name__ == '__main__':
             print("\tStart address: ", hex(e["saddr"]))
             print("\tEnd address: ", hex(e["eaddr"]))
         if "loc" in e:
-            print("\r\tWriting address: ", hex(e["loc"]), end="")
+            if e["resend"] > 0:
+                end = "\n"
+            else:
+                end = ""
+            print("\r\tWriting address: %s --- %d" % (hex(e["loc"]), e["resend"]), end=end)
             sys.stdout.flush()
 
-    print("\n\tDone")
-
-
-# if data == ACK:
-#   print("Board is ready to accept commands")
-
-# ih = IntelHex()  
-# ih.loadhex(sys.argv[2])
-# print(dir(ih))
-# print hex(ih.minaddr())
-# print hex(ih.maxaddr())
-# addr = ih.minaddr()
-# content = ih.todict()
-# while addr <= ih.maxaddr():
-#   data = []
-#   for i in range(16):
-#       try:
-#           # print "Programming: ", hex(addr),
-#           # print " Content: ", hex(content[addr])
-#           data.append(content[addr])
-#       except KeyError:
-#           data.append(0xFF)
-#       addr+=1
-
-#   print data
-#   data = AESEncryptor.encrypt("".join(map(chr,data)))
-#   #data = AESEncryptor.decrypt(data)
-#   #print map(ord, data)
-#   ser.write(data)
-#   time.sleep(0.01)
-# content = ih.todict()
-# for k in content.keys():
-#   try:
-#       print hex(k), hex(content[k])
-#   except:
-#       pass
+        if "success" in e and e["success"]:
+            print("\n\tDone")
+        elif "success" in e and not e["success"]:
+            print("\n\tFailed to upload firmware")
--- nucleo-f401RE/src/ch16/main-bootloader.c
+++ nucleo-f401RE/src/ch16/main-bootloader.c
@@ -5,36 +5,36 @@
 #include "TI_aes_128.h"
 
 /* Global macros */
-
-#define FALSE 0
-#define TRUE 1
-
 #define ACK 0x79
 #define NACK 0x1F
-
-#define CMD_ENTER_PROGRAM_MODE 0x7f
-#define CMD_ERASE              0x43
-#define CMD_GETID              0x02
-#define CMD_WRITE              0x2b
-
-#define SRAM_SIZE       96*1024     // STM32F401RE has 96 KB of RAM
-#define SRAM_END        (SRAM_BASE + SRAM_SIZE)
+#define CMD_ERASE         0x43
+#define CMD_GETID         0x02
+#define CMD_WRITE         0x2b
 
 #define APP_START_ADDRESS 0x08004000 /* In STM32F401RE this corresponds with the start
                                         address of Sector 1 */
 
+#define SRAM_SIZE         96*1024     // STM32F401RE has 96 KB of RAM
+#define SRAM_END          (SRAM_BASE + SRAM_SIZE)
+
+#define ENABLE_BOOTLOADER_PROTECTION 0
 /* Private variables ---------------------------------------------------------*/
+//The AES_KEY cannot be defined const, because the aes_enc_dec() function temporarily
+//modifies its content
 uint8_t AES_KEY[] = { 0x4D, 0x61, 0x73, 0x74, 0x65, 0x72, 0x69, 0x6E, 0x67,
     0x20, 0x20, 0x53, 0x54, 0x4D, 0x33, 0x32 };
+
 extern CRC_HandleTypeDef hcrc;
 extern UART_HandleTypeDef huart2;
 
 /* Private function prototypes -----------------------------------------------*/
 void _start(void);
+void CHECK_AND_SET_FLASH_PROTECTION(void);
 void cmdErase(uint8_t *data);
 void cmdGetID(uint8_t *data);
 void cmdWrite(uint8_t *data);
 int main(void);
+void MX_CRC_Init(void);
 void MX_GPIO_Init(void);
 void MX_USART2_UART_Init(void);
 void SysTick_Handler();
@@ -87,155 +87,237 @@ _start(void) {
     ;
 }
 
+
 int main(void) {
   uint32_t ticks = HAL_GetTick();
   uint8_t data[20];
-  uint8_t inProgramMode = FALSE;
-  UNUSED(AES_KEY);
 
   HAL_Init();
   MX_GPIO_Init();
 
+  /* Ensures that the first sector of flash is write-protected preventing that the
+   bootloader is overwritten */
+#if ENABLE_BOOTLOADER_PROTECTION
+  CHECK_AND_SET_FLASH_PROTECTION();
+#endif
+
+  /* If USER_BUTTON is pressed */
   if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET) {
-//  if(1) {
+    /* We set so that the SysTick timer overflows ever 1ms */
     HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
     HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-    MX_CRC_Init();
+    MX_CRC_Init(); /* CRC module enabled */
     MX_USART2_UART_Init();
 
     ticks = HAL_GetTick();
 
     while (1) {
+      /* Every 500ms the LD2 LED blinks, so that we can check that the bootloader
+       is running. */
       if (HAL_GetTick() - ticks > 500) {
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
         ticks = HAL_GetTick();
       }
 
-      HAL_UART_Receive(&huart2, data, 1, 10);
-
+      /* We check for new commands arriving on the UART2 */
+      HAL_UART_Receive(&huart2, data, 20, 10);
       switch (data[0]) {
-      case CMD_ENTER_PROGRAM_MODE:
-        inProgramMode = TRUE;
-        data[0] = ACK;
-        HAL_UART_Transmit(&huart2, data, 1, HAL_MAX_DELAY);
-        break;
       case CMD_GETID:
-        if (inProgramMode) {
-          cmdGetID(data);
-        } else {
-          data[0] = NACK;
-          HAL_UART_Transmit(&huart2, data, 1, HAL_MAX_DELAY);
-        }
+        cmdGetID(data);
         break;
       case CMD_ERASE:
-        if (inProgramMode) {
-          cmdErase(data);
-        } else {
-          data[0] = NACK;
-          HAL_UART_Transmit(&huart2, data, 1, HAL_MAX_DELAY);
-        }
+        cmdErase(data);
         break;
       case CMD_WRITE:
-        if (inProgramMode) {
-          cmdWrite(data);
-        } else {
-          data[0] = NACK;
-          HAL_UART_Transmit(&huart2, data, 1, HAL_MAX_DELAY);
-        }
+        cmdWrite(data);
         break;
       };
     }
-  }
-
-  while (1) {
-//    HAL_Delay(500);
-//    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+  } else {
+    /* USER_BUTTON is not pressed. We first check if the second sector is erased. If so,
+     the LD2 LED blinks quickly. */
     if (*((uint32_t*) APP_START_ADDRESS) == 0xFFFFFFFF) {
       while (1) {
         HAL_Delay(30);
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
       }
     } else {
-      SCB->VTOR = APP_START_ADDRESS;
-      __set_MSP(SRAM_END);
-      RCC->CIR = 0x00000000;
+      /* A valid program seems to exist in the second sector: we so prepare the MCU to start
+       the main firmware */
+      SCB->VTOR = APP_START_ADDRESS; //We relocate vector table to the sector 1
+      __set_MSP(SRAM_END); //Set the MSP to the end of SRAM
+      RCC->CIR = 0x00000000; //Disable all interrupts related to clock
       uint32_t JumpAddress = *(__IO uint32_t*) (0x08004000 + 4);
       void (*reset_handler)(void) = (void*)JumpAddress;
-      reset_handler();
+      reset_handler(); //We start the execution from he Reset_Handler of the main firmware
+
+      for (;;)
+        ; //Never coming here
     }
   }
 }
 
+/* Performs a flash erase of a given number of sectors/pages.
+ *
+ * An erase command has the following structure:
+ *
+ * ----------------------------------------
+ * | CMD_ID | # of sectors      |  CRC32  |
+ * | 1 byte |     1 byte        | 4 bytes |
+ * |--------|-------------------|---------|
+ * |  0x43  | N or 0xFF for all |   CRC   |
+ * ----------------------------------------
+ */
 void cmdErase(uint8_t *data) {
   FLASH_EraseInitTypeDef eraseInfo;
-  uint32_t badBlocks = 0;
-  UNUSED(data);
+  uint32_t badBlocks = 0, crc = 0;
+  uint32_t cmd[] = { data[0], data[1] };
 
-  eraseInfo.Banks = FLASH_BANK_1;
-  eraseInfo.NbSectors = 1;  //FLASH_SECTOR_TOTAL - 1;
-  eraseInfo.Sector = FLASH_SECTOR_1;
-  eraseInfo.TypeErase = FLASH_TYPEERASE_SECTORS;
-  eraseInfo.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+  memcpy(&crc, data + 2, sizeof(uint32_t));
 
-  HAL_FLASH_Unlock();
-  HAL_FLASHEx_Erase(&eraseInfo, &badBlocks);
-  HAL_FLASH_Lock();
+  /* Checks if provided CRC is correct */
+  if (crc == HAL_CRC_Calculate(&hcrc, cmd, 2)) {
+    /* If data[1] contains 0xFF, it deletes all sectors; otherwise
+     * the number of sectors specified. */
+    eraseInfo.Banks = FLASH_BANK_1;
+    eraseInfo.NbSectors = data[1] == 0xFF ? FLASH_SECTOR_TOTAL - 1 : data[1];
+    eraseInfo.Sector = FLASH_SECTOR_1;
+    eraseInfo.TypeErase = FLASH_TYPEERASE_SECTORS;
+    eraseInfo.VoltageRange = FLASH_VOLTAGE_RANGE_3;
 
-  data[0] = ACK;
-  HAL_UART_Transmit(&huart2, (uint8_t *) data, 1, HAL_MAX_DELAY);
+    HAL_FLASH_Unlock(); //Unlocks the flash memory
+    HAL_FLASHEx_Erase(&eraseInfo, &badBlocks); //Deletes given sectors */
+    HAL_FLASH_Lock(); //Locks again the flash memory
 
-  for (uint8_t i = 0; i < 10; i++) {
-    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-    HAL_Delay(50);
+    /* Sends an ACK */
+    data[0] = ACK;
+    HAL_UART_Transmit(&huart2, (uint8_t *) data, 1, HAL_MAX_DELAY);
+  } else {
+    /* The CRC is wrong: sends a NACK */
+    data[0] = NACK;
+    HAL_UART_Transmit(&huart2, data, 1, HAL_MAX_DELAY);
+  }
+
+}
+
+/* Retrieve the STM32 MCU ID
+ *
+ * A GET_ID command has the following structure:
+ *
+ * --------------------
+ * | CMD_ID |  CRC32  |
+ * | 1 byte | 4 bytes |
+ * |--------|---------|
+ * |  0x02  |   CRC   |
+ * --------------------
+ */
+void cmdGetID(uint8_t *data) {
+  uint16_t devID;
+  uint32_t crc = 0;
+  uint32_t cmd = data[0];
+
+  memcpy(&crc, data + 1, sizeof(uint32_t));
+
+  /* Checks if provided CRC is correct */
+  if (crc == HAL_CRC_Calculate(&hcrc, &cmd, 1)) {
+    devID = (uint16_t) (DBGMCU->IDCODE & 0xFFF); //Retrieves MCU ID from DEBUG interface
+
+    /* Sends an ACK */
+    data[0] = ACK;
+    HAL_UART_Transmit(&huart2, data, 1, HAL_MAX_DELAY);
+
+    /* Sends the MCU ID */
+    HAL_UART_Transmit(&huart2, (uint8_t *) &devID, 2, HAL_MAX_DELAY);
+  } else {
+    /* The CRC is wrong: sends a NACK */
+    data[0] = NACK;
+    HAL_UART_Transmit(&huart2, data, 1, HAL_MAX_DELAY);
   }
 }
 
-void cmdGetID(uint8_t *data) {
-  UNUSED(data);
-  uint16_t devID = (uint16_t) (DBGMCU->IDCODE & 0xFFF);
-
-  HAL_UART_Transmit(&huart2, (uint8_t *) &devID, 2, HAL_MAX_DELAY);
-}
-
+/* Performs a write of 16 bytes starting from the specified address.
+ *
+ * A write command has the following structure:
+ *
+ * ----------------------------------------
+ * | CMD_ID | starting address  |  CRC32  |
+ * | 1 byte |     4 byte        | 4 bytes |
+ * |--------|-------------------|---------|
+ * |  0x2b  |    0x08004000     |   CRC   |
+ * ----------------------------------------
+ *
+ * The second message has the following structure
+ *
+ * ------------------------------
+ * |    data bytes    |  CRC32  |
+ * |      16 bytes    | 4 bytes |
+ * |------------------|---------|
+ * | BBBBBBBBBBBBBBBB |   CRC   |
+ * ------------------------------
+ */
 void cmdWrite(uint8_t *data) {
-  uint32_t addr = APP_START_ADDRESS;
-  uint32_t msgContent = 0, crc = 0;
-  uint32_t *data32 = (uint32_t*) data;
-  uint8_t dn[20];
+  uint32_t saddr = 0, crc = 0;
 
-  data[0] = ACK;
-  HAL_UART_Transmit(&huart2, (uint8_t *) data, 1, HAL_MAX_DELAY);
+  memcpy(&saddr, data + 1, sizeof(uint32_t));
+  memcpy(&crc, data + 5, sizeof(uint32_t));
 
-  HAL_FLASH_Unlock();
-  while (1) {
-    HAL_UART_Receive(&huart2, data, 20, HAL_MAX_DELAY);
+  uint32_t data32[5];
+  for(int i = 0; i < 5; i++)
+    data32[i] = data[i];
 
-    msgContent = 0;
-    memcpy(&crc, data32 + 4, sizeof(uint32_t));
-    memcpy(dn, data, sizeof(uint8_t)*20);
-
-    if (crc == HAL_CRC_Calculate(&hcrc, data32, 4)) {
-      aes_enc_dec((uint8_t*) data32, AES_KEY, 1);
-
-      for (uint8_t i = 0; i < 4; i++)
-        msgContent += data32[i];
-
-      if (msgContent) {
-        for (uint8_t i = 0; i < 4; i++) {
-          HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, addr, data32[i]);
-          HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-          addr += 4;
-        }
-      }
-      data[0] = ACK;
-    } else {
-      data[0] = NACK;
-    }
-
+  /* Checks if provided CRC is correct */
+  if (crc == HAL_CRC_Calculate(&hcrc, data32, 5)) {
+    /* Sends an ACK */
+    data[0] = ACK;
     HAL_UART_Transmit(&huart2, (uint8_t *) data, 1, HAL_MAX_DELAY);
 
-    if (!msgContent)
+    /* Now retrieves given amount of bytes plus the CRC32 */
+    if (HAL_UART_Receive(&huart2, data, 16 + 4, 200) == HAL_TIMEOUT)
       return;
+
+    memcpy(&crc, data + 16, sizeof(uint32_t));
+
+    /* Checks if provided CRC is correct */
+    if (crc == HAL_CRC_Calculate(&hcrc, (uint32_t*) data, 4)) {
+      HAL_FLASH_Unlock(); //Unlocks the flash memory
+
+      /* Decode the sent bytes using AES-128 ECB */
+      aes_enc_dec((uint8_t*) data, AES_KEY, 1);
+      for (uint8_t i = 0; i < 16; i++) {
+        /* Store each byte in flash memory starting from the specified address */
+        HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE, saddr, data[i]);
+        saddr += 1;
+      }
+      HAL_FLASH_Lock(); //Locks again the flash memory
+
+      /* Sends an ACK */
+      data[0] = ACK;
+      HAL_UART_Transmit(&huart2, (uint8_t *) data, 1, HAL_MAX_DELAY);
+    } else {
+      goto sendnack;
+    }
+  } else {
+    goto sendnack;
+  }
+
+  sendnack: data[0] = NACK;
+  HAL_UART_Transmit(&huart2, (uint8_t *) data, 1, HAL_MAX_DELAY);
+}
+
+void CHECK_AND_SET_FLASH_PROTECTION(void) {
+  FLASH_OBProgramInitTypeDef obConfig;
+
+  /* Retrieves current OB */
+  HAL_FLASHEx_OBGetConfig(&obConfig);
+
+  /* If the first sector is not protected */
+  if ((obConfig.WRPSector & OB_WRP_SECTOR_0) == OB_WRP_SECTOR_0) {
+    HAL_FLASH_OB_Unlock(); //Unlocks OB
+    obConfig.WRPState = OB_WRPSTATE_ENABLE; //Enables changing of WRP settings
+    obConfig.WRPSector = OB_WRP_SECTOR_0; //Enables WP on first sector
+    HAL_FLASHEx_OBProgram(&obConfig); //Programs the OB
+    HAL_FLASH_OB_Launch(); //Ensures that the new configuration is saved in flash
+    HAL_FLASH_OB_Lock(); //Locks OB
   }
 }
 
