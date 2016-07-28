304dc0748b65d22fdedfd214253053d4135f6f7a
Carmine Noviello <cnoviello@hotmail.com>
2016-07-28 12:19:15 +0200

    Ported CH16 examples to L053

--- nucleo-l053R8/system/include/stm32l0xx/stm32_assert_template.h
+++ nucleo-l053R8/system/include/stm32l0xx/stm32_assert_template.h
@@ -2,8 +2,8 @@
   ******************************************************************************
   * @file    stm32_assert.h
   * @author  MCD Application Team
-  * @version V1.5.0
-  * @date    8-January-2016
+  * @version V1.7.0
+  * @date    31-May-2016
   * @brief   STM32 assert template file.
   *          This file should be copied to the application folder and renamed
   *          to stm32_assert.h.
@@ -58,11 +58,11 @@
   *         If expr is true, it returns no value.
   * @retval None
   */
-  #define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
+  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
 /* Exported functions ------------------------------------------------------- */
   void assert_failed(uint8_t* file, uint32_t line);
 #else
-  #define assert_param(expr) ((void)0)
+  #define assert_param(expr) ((void)0U)
 #endif /* USE_FULL_ASSERT */
 
 #ifdef __cplusplus
